f1d1c30bdc087324101bd2b24eb6cc059638f16c
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-06-12 13:11:47 +0200

    dd

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+//call to all entries endpoint
+//fetch list of all jokes
+//show setup and type properties and btn to view punchline
+//btns to filter entries by general or programming
---
+++ javascript/joke.js
@@ -0,0 +1,4 @@
+//call to single entries endpoint
+//show type, setup, punchline
+//hide punchline originally
+//btn to reveal punchline, btn to hide punchline when its revealed
